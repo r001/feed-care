72a3ba62a6a58c5bf578c4f863241872ad8d7b14
root <root@vox.tilaa.cloud>
2017-11-24 02:00:07 +0100

    hostname changed

--- alert_ethereum.sh
+++ alert_ethereum.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "$DIR/alert_settings.sh"
 ERROR=0
-BLOCK_AGE_FILE=$DIR/block.age
+BLOCK_AGE_FILE="$DIR/block.age"
 if BLOCK_AGE=$(/bin/date --date="`/usr/local/bin/seth age`" +"%s"); then 
 	if [ $BLOCK_AGE -gt $(/bin/cat $BLOCK_AGE_FILE)  ]; then
 		echo $BLOCK_AGE > $BLOCK_AGE_FILE
