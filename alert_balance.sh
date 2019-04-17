a071450272d5078d65e1d7b191ce33348199aa9a
r001 <rob@nyar.eu>
2019-04-17 19:58:26 +0200

    bugfixes, and account unlocking replaced by raw tx sending

--- alert_balance.sh
+++ alert_balance.sh
@@ -4,12 +4,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "$DIR/alert_lib.sh"
 getnode
 . "$DIR/alert.conf"
-BALANCE=`/usr/local/bin/seth balance $ACC |/bin/sed  s/E.*//g`
+BALANCE="$(/usr/local/bin/seth balance $ACC |/bin/sed  s/E.*//g)"
 if (( $(echo "$BALANCE < $THRESHOLD" | bc -l)  )); then 
 	printf "Alert from VOX!\r\n\r\n\
 Balance is $BALANCE and it is less than $THRESHOLD .\r\n\
 Please deposit into account $ACC !\r\n\r\n\
 Best regards\r\n\
-cron@"`hostname`":$DIR/alert_balance.sh"\
+cron@"$(hostname)":$DIR/alert_balance.sh"\
 |/usr/bin/mail -a "X-Priority:1" -s "Vox alert - balance low ($BALANCE ETH)!" $EMAIL
 fi
--- alert_filesystem.sh
+++ alert_filesystem.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "$DIR/alert.conf"
-DOMAINNAME=`hostname`
-USEDSPACE=`/bin/df|/bin/grep vda1|/bin/sed 's/.*\s\([0-9]\+\)%.*/\1/g'`
-if (( $(echo "$USEDSPACE >= $DISK_SPACE_THRESHOLD" | bc -l)  )); then 
+DOMAINNAME="$(hostname)"
+USEDSPACE="$(/bin/df|/bin/grep vda1|/bin/sed 's/.*\s\([0-9]\+\)%.*/\1/g')"
+if (( "$(echo "$USEDSPACE >= $DISK_SPACE_THRESHOLD" | bc -l)"  )); then 
 	printf "Alert from VOX!\r\n\r\n\
 Disk usage is $USEDSPACE percent, please enlarge disk space for  $DOMAINNAME!\r\n\r\n\
 Best regards\r\n\
