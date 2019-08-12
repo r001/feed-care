2bdd023f0a88536e4eb7c099e7a13dea9f68fc5d
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2019-08-12 15:07:26 +0300

    Create default block_parity.age file on paity/geth check

--- alert_parity.sh
+++ alert_parity.sh
@@ -5,18 +5,22 @@ DIR=/root/.scripts
 . "$DIR/alert.conf"
 . "/etc/setzer.conf"
 ERROR=0
-BLOCK_AGE_FILE="$DIR/block_parity.age" 
+BLOCK_AGE_FILE="$DIR/block_parity.age"
+if [ ! -f $BLOCK_AGE_FILE ]; then
+	echo 0 > $BLOCK_AGE_FILE
+fi
 export ETH_RPC_PORT=$PARITY_RPC_PORT
-BLOCK_AGE=$(/bin/date --date="`timeout $RPC_TIMEOUT /usr/local/bin/seth age`" +"%s" || echo 0) 
+BLOCK_AGE=$(/bin/date --date="`timeout $RPC_TIMEOUT /usr/local/bin/seth age`" +"%s" || echo 0)
+echo $BLOCK_AGE > $BLOCK_AGE_FILE
 	if [ $BLOCK_AGE -gt $(/bin/cat $BLOCK_AGE_FILE)  ]; then
 		echo $BLOCK_AGE > $BLOCK_AGE_FILE
 		log "Parity client is fine."
-	else 
+	else
 #	printf "Alert from VOX!\r\n\r\n\
 #Parity is unavailable. Restarting parity.\r\n\r\n\
 #Best regards\r\n\
 #cron@"`hostname`":/root/.scripts/alert_parity.sh"\
 #|/usr/bin/mail -a "X-Priority:1" -s "Vox alert - parity is down!" $EMAIL
-       	/usr/sbin/service parity restart
+		/usr/sbin/service parity restart
 	log "Error: parity was restarted."
 fi
